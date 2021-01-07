dd34cae822b823bb7e8e567385dbdbd374f1f8d5
Iuri C Freire <iuricostafreire@gmail.com>
2021-01-06 22:21:20 -0300

    1st commit

---
+++ codigo.py
@@ -0,0 +1,22 @@
+print('''
+Selecione uma opção do menu:
+
+Menu:
+1. Somar
+2. Cálcular IMC
+''')
+
+opcao = int(input('Opção: '))
+
+if opcao == 1:
+	num1 = int(input('Primeiro número: '))
+	num2 = int(input('Segundo número: '))
+	print(f'O resultado da soma de {num1} e {num2} é {num1 + num2}.')
+elif opcao == 2:
+	peso = int(input('Peso em kg: '))
+	altura = float(input('Altura em metros: '))
+	print(f'O IMC é {round(peso / altura ** 2, 2)}.')
+else:
+	print('Opção inválida')
+
+print('')
